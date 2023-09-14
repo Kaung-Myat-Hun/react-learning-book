91c76979a7445bae45791e83e0d2f738c3f641c4
kaung <kaungmyathun7@gmail.com>
2023-09-14 17:33:56 +0630

    adding: project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Child from "./Child"
+
+function App() {
+
+  return (
+    <>
+      <Child name="Bate Thar"></Child>
+    </>
+  )
+}
+
+export default App
---
+++ src/Child.jsx
@@ -0,0 +1,59 @@
+
+// function Child({name}) {
+//   return (
+//     <h1>{name}</h1>
+//   )
+// }
+
+
+
+import {Component} from 'react'
+class Child extends Component {
+constructor(props) {
+super(props)
+this.state= {
+  count : 0
+}
+}
+clickHandler = () => {
+  this.setState({
+    count : this.state.count + 1
+  })
+}
+addFiveHandler = () =>{
+  for(let i =0; i<5; i++){
+    console.log("adding" , i)
+    this.setState((prev) => {
+      return {
+        count: prev.count + 1 
+      }
+    })
+  }
+}
+render(){
+  return(
+    <>
+      <h1>{this.state.count}</h1>
+      <button onClick={this.clickHandler}>Add One</button>
+      <button onClick={this.addFiveHandler}>Add five</button>
+    </>
+    )
+  }
+}
+export default Child
+
+
+// import React, { Component } from 'react'
+
+// class Child extends Component {
+//   constructor(props) {
+//     super(props)
+//   }
+//   render() {
+//     return (
+//       <div>Child</div>
+//     )
+//   }
+// }
+
+// export default Child
